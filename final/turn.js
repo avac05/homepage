7adb2822f234db7edb919c1dfdc57ac7a0daba67
avac05 <craftam2@vcu.edu>
2025-12-04 17:54:52 -0500

    final pt5

---
+++ final/turn.js
@@ -0,0 +1,19 @@
+const imgwrapper = document.querySelector('.img-wrapper')
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+let a = 0;
+
+prev.addEventListener("click", function(){
+    a = a +45;
+    updatefun();
+});
+
+next.addEventListener("click", function(){
+    a = a -45;
+    updatefun();
+});
+
+function updatefun(){
+    imgwrapper.style.transform = `perspective(1000px)rotateY(${a}deg)`
+}
